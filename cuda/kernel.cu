c6ef54496db836ca8bddc0e62da76a970c6a91fe
Ben Lancaster <bdl@live.co.uk>
2017-12-09 18:46:43 +0000

    cuda/test: Output vector to file and plot input and output
    Signed-off-by: Ben Lancaster <bdl@live.co.uk>

--- cuda/kernel.cu
+++ cuda/kernel.cu
@@ -28,11 +28,15 @@ int assert_vec(double *v, double *vt, int n)
         return 1;
 }
 
-void print_vec(double *v, int n)
+void print_vec(FILE *f, double *v, int n)
 {
         int i;
+        FILE *f_out;
+
+        if (f) f_out = f;
+        else f_out = stdout;
         for (i = 0; i < n; i++) {
-                printf("%d.0 %.2lf\n", i, v[i]);
+                fprintf(f_out, "%d,%.2lf\n", i, v[i]);
         }
 }
 
@@ -95,6 +99,7 @@ int read_into_v(char *path, double **v, int *vn)
         }
 }
 
+// Sequential implementation of the DFT algorithm
 int seq_dft(
         _in_ double *x, _in_ int xn,
         _out_ double **fx)
@@ -123,7 +128,12 @@ int seq_dft(
 
 __global__ void kernel_dft(int xn, double *a, double *q)
 {
+        int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
+        if (idx > xn) return;
+
+        printf("%d\r\n", idx);
+        q[idx] = a[idx];
 }
 
 cudaError_t cu_dft(
@@ -134,38 +144,76 @@ cudaError_t cu_dft(
         double *dev_q = NULL;
         cudaError_t cuda_status;
 
-        int block_size = 256;
+        int block_size = 512;
         int num_blocks = (xn + block_size - 1) / block_size;
         printf("block_size: %d num_blocks: %d xn: %d\r\n",
                 block_size, num_blocks, xn);
 
+        // Allocate host output memory
+        *fx = (double*)calloc(xn, sizeof(double));
+
         // Allocate GPU buffers for two vectors (one input, one output)
-        cuda_status = cudaMalloc((void**)&dev_x, xn * sizeof(double));
-        cuda_status = cudaMalloc((void**)&dev_q, xn * sizeof(double));
+        cuda_status = cudaMalloc(&dev_x, xn * sizeof(double));
+        cuda_status = cudaMalloc(&dev_q, xn * sizeof(double));
 
         // Copy input vectors from host memory to GPU buffers.
-        cuda_status = cudaMemcpy(dev_x, x, xn * sizeof(float), cudaMemcpyHostToDevice);
+        cuda_status = cudaMemcpy(dev_x, x, xn * sizeof(double), cudaMemcpyHostToDevice);
 
+        kernel_dft <<<num_blocks, block_size>>>(xn, dev_x, dev_q);
 
+        // Check for any errors launching the kernel
+        cuda_status = cudaGetLastError();
+        if (cuda_status != cudaSuccess) {
+                printf("addKernel launch failed: %s\n", cudaGetErrorString(cuda_status));
+                goto error;
+        }
+        
+        // Copy output vector from GPU buffer to host memory.
+        cuda_status = cudaMemcpy(*fx, dev_q, xn * sizeof(double), cudaMemcpyDeviceToHost);
+        if (cuda_status != cudaSuccess) {
+                printf("cudaMemcpy failed!");
+                goto error;
+        }
+
+error:
+        cudaFree(dev_x);
+        cudaFree(dev_q);
 
         return cuda_status;
 }
 
 int main()
 {
+        FILE    *out_dft; // Output file
+
         // Hyper parameters
-        double *vt, *vf;
-        int samples;
-        int i;
+        double  *sf, // sequentual dft f(x)
+                *vt, // value 
+                *vf; // cuda dft f(x)
+        int     samples;
+        int     i;
 
         read_into_v("../data/square.csv", &vt, &samples);
+        read_into_v("../data/square.csv", &sf, &samples);
 
-        seq_dft(vt, samples, &vf);
-        print_vec(vf, samples);
+        out_dft = fopen("../test/dft.txt", "w");
+        if (!out_dft) {
+                printf("Error creating output dft file!\r\n");
+                goto exit;
+        }
+
+        seq_dft(vt, samples, &sf);
+        print_vec(out_dft, sf, samples);
 
         cu_dft(vt, samples, &vf);
 
-        printf("assert_vec: %d\r\n", assert_vec(vt, vf, samples));
+        printf("assert_vec: %s\r\n", 
+                assert_vec(vt, vf, samples)? "SUCCESS" : "FAIL");
+/*
+        for (i = 0; i < samples; i++) {
+                printf("%d\t%f\t%f\t%s\r\n", i, vt[i], vf[i],
+                        vt[i] == vf[i] ? "" : "!!!");
+        }*/
 
 exit:
         free(vt);
