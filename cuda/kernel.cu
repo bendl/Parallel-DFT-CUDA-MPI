04557cdb81bcbcafc800dae9ade332503e5443c7
Ben Lancaster <bdl@live.co.uk>
2018-01-13 17:41:49 +0000

    project: Remove util project
    Signed-off-by: Ben Lancaster <bdl@live.co.uk>

--- cuda/kernel.cu
+++ cuda/kernel.cu
@@ -16,12 +16,105 @@
 #define M_PI 3.14159265358979323846
 #include <time.h>
 #include <string.h>
+#include <windows.h>
 
 #include "../util/soft354_file.h"
 
 #define _in_
 #define _out_
 #define _inout_
+
+int read_get_lines(char *path)
+{
+        FILE *f;
+        int ch, nlines = 0;
+
+        f = fopen(path, "r");
+        if (!f) {
+                return 0;
+        }
+
+        while ((ch = fgetc(f)) != EOF) {
+                if (ch == '\n') {
+                        nlines++;
+                }
+        }
+
+        fclose(f);
+
+        return nlines;
+}
+
+int read_into_v(char *path, double **v, int *vn)
+{
+        FILE *f;
+        char line_buf[256];
+        char *lp;
+
+        int line = 0;
+        int col = 0;
+        int i = 0;
+        int j = 0;
+
+        *vn = read_get_lines(path);
+        *v = (double*)calloc(*vn, sizeof(double));
+
+        f = fopen(path, "r");
+        if (!f) {
+                return 0;
+        }
+
+        while (fgets(line_buf, 256, f)) {
+                if (++line > 2) {
+                        lp = strtok(line_buf, ",");
+                        while (lp != NULL) {
+                                if (col == 1) {
+                                        double val = -1;
+                                        //printf("%s\r\n", lp);
+                                        if (sscanf(lp, "%lf", &val) == 1) {
+                                                (*v)[i++] = val;
+                                        }
+                                }
+                                lp = strtok(NULL, ",");
+                                col++;
+                        }
+                        col = 0;
+                }
+        }
+}
+
+void fprint_vec(FILE *f, double *v, int n)
+{
+        int i;
+        FILE *f_out;
+
+        if (f) f_out = f;
+        else f_out = stdout;
+        for (i = 0; i < n; i++) {
+                fprintf(f_out, "%d,%.2lf\n", i, v[i]);
+        }
+}
+
+void timer_start(__int64 * start, double *freq)
+{
+        LARGE_INTEGER li;
+
+        if (!QueryPerformanceFrequency(&li)) return;
+
+        *freq = (double)(li.QuadPart / 1000.0);
+
+        QueryPerformanceCounter(&li);
+        *start = li.QuadPart;
+}
+
+double timer_stop(__int64 * start, double *freq)
+{
+        LARGE_INTEGER li;
+        QueryPerformanceCounter(&li);
+        return (double)(li.QuadPart - *start) / *freq;
+}
+
+
 
 int assert_vec(double *v, double *vt, int n)
 {
@@ -146,7 +239,7 @@ int main()
         //print_vec(out_dft, sf, samples);
 
         cu_dft(vt, samples, &vf);
-        print_vec(out_dft, sf, samples);
+        fprint_vec(out_dft, sf, samples);
 
         printf("assert_vec: %s\r\n", 
                 assert_vec(sf, vf, samples)? "SUCCESS" : "FAIL");
--- mpi/main.c
+++ mpi/main.c
@@ -70,6 +70,98 @@ double  time_gather;
         ROOT_ONLY { printf("%d\t" #out "\t%f ms\r\n", world_rank, out); }
         
 
+int read_get_lines(char *path)
+{
+        FILE *f;
+        int ch, nlines = 0;
+
+        f = fopen(path, "r");
+        if (!f) {
+                return 0;
+        }
+
+        while ((ch = fgetc(f)) != EOF) {
+                if (ch == '\n') {
+                        nlines++;
+                }
+        }
+
+        fclose(f);
+
+        return nlines;
+}
+
+int read_into_v(char *path, double **v, int *vn)
+{
+        FILE *f;
+        char line_buf[256];
+        char *lp;
+
+        int line = 0;
+        int col = 0;
+        int i = 0;
+        int j = 0;
+
+        *vn = read_get_lines(path);
+        *v = (double*)calloc(*vn, sizeof(double));
+
+        f = fopen(path, "r");
+        if (!f) {
+                return 0;
+        }
+
+        while (fgets(line_buf, 256, f)) {
+                if (++line > 2) {
+                        lp = strtok(line_buf, ",");
+                        while (lp != NULL) {
+                                if (col == 1) {
+                                        double val = -1;
+                                        //printf("%s\r\n", lp);
+                                        if (sscanf(lp, "%lf", &val) == 1) {
+                                                (*v)[i++] = val;
+                                        }
+                                }
+                                lp = strtok(NULL, ",");
+                                col++;
+                        }
+                        col = 0;
+                }
+        }
+}
+
+void fprint_vec(FILE *f, double *v, int n)
+{
+        int i;
+        FILE *f_out;
+
+        if (f) f_out = f;
+        else f_out = stdout;
+        for (i = 0; i < n; i++) {
+                fprintf(f_out, "%d,%.2lf\n", i, v[i]);
+        }
+}
+
+void timer_start(__int64 * start, double *freq)
+{
+        LARGE_INTEGER li;
+
+        if (!QueryPerformanceFrequency(&li)) return;
+
+        *freq = (double)(li.QuadPart / 1000.0);
+
+        QueryPerformanceCounter(&li);
+        *start = li.QuadPart;
+}
+
+double timer_stop(__int64 * start, double *freq)
+{
+        LARGE_INTEGER li;
+        QueryPerformanceCounter(&li);
+        return (double)(li.QuadPart - *start) / *freq;
+}
+
+
+
 // Sequential implementation of the DFT algorithm
 int seq_dft(
         _in_ double *x, _in_ int xn,
